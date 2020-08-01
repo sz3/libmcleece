7ac4f03b048159f73bcb3f1b99b02885c8496a77
sz <sz@galacticicecube.com>
2020-08-01 05:52:49 -0500

    Temporarily use std::random_device,
    because possibly-fake randomness is ok for now

--- src/exe/cmick/main.cpp
+++ src/exe/cmick/main.cpp
@@ -1,7 +1,12 @@
 
+extern "C" {
+#include "mceliece348864/nist/rng.h"
+}
+
 #include "mceliece348864/crypto_kem.h"
 #include <iostream>
 #include <fstream>
+#include <random>
 #include <vector>
 using std::vector;
 
@@ -11,13 +16,32 @@ namespace {
 		std::ofstream of(filename);
 		of.write(reinterpret_cast<const char*>(contents.data()), contents.size());
 	}
+
+	void init_rng()
+	{
+		// random_device offers poor randomness guarantees, unfortunately -- so it's not really a solution here.
+		// but for the moment it will do
+		std::random_device randomEngine;
+		vector<unsigned char> seed(48, 0);
+		for (int count = 0; count < seed.size();)
+		{
+			unsigned rng = randomEngine();
+			for (int i = 0; i < sizeof(unsigned) and count < seed.size(); ++i, ++count)
+			{
+				seed[count] = rng & 0xFF;
+				rng = rng >> 8;
+			}
+		}
+		randombytes_init(seed.data(), NULL, 0);
+	}
 }
 
 int main()
 {
-// pk, sk
-	vector<unsigned char> pk(crypto_kem_mceliece348864_ref_PUBLICKEYBYTES, 0);
-	vector<unsigned char> sk(crypto_kem_mceliece348864_ref_SECRETKEYBYTES, 0);
+	init_rng();
+
+	vector<unsigned char> pk(crypto_kem_mceliece348864_ref_PUBLICKEYBYTES);
+	vector<unsigned char> sk(crypto_kem_mceliece348864_ref_SECRETKEYBYTES);
 	int res = crypto_kem_keypair(pk.data(), sk.data());
 	std::cout << "hello" << res << std::endl;
 
