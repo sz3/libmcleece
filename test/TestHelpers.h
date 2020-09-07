11e792b774a394c6715886cf21c728e9f443fa25
sz <sz@galacticicecube.com>
2020-09-07 08:02:24 -0500

    Experimenting with a way to run tests + reuse keypairs

---
+++ test/TestHelpers.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "mcleece/keygen.h"
+#include "serialize/format.h"
+#include "util/File.h"
+#include <experimental/filesystem>
+#include <string>
+
+namespace TestHelpers
+{
+	inline bool generate_keypair(std::experimental::filesystem::path target_prefix)
+	{
+		std::string basename = std::experimental::filesystem::path(target_prefix).filename();
+		std::string pk = fmt::format("/tmp/{}.pk", basename);
+		std::string sk = fmt::format("/tmp/{}.sk", basename);
+		if (File(pk).good() and File(sk).good())
+		{
+			std::experimental::filesystem::copy(pk, target_prefix.replace_extension("pk"));
+			std::experimental::filesystem::copy(sk, target_prefix.replace_extension(".sk"));
+			return false;
+		}
+		else
+			mcleece::generate_keypair(target_prefix.replace_extension(".pk"), target_prefix.replace_extension(".sk"), "password");
+		return true;
+	}
+}
+
