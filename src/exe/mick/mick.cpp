ab7c3ba7451c93b3889352c8e32728fb16a66efb
sz <sz@galacticicecube.com>
2020-11-19 02:54:09 -0600

    Update error msg

--- src/exe/mick/mick.cpp
+++ src/exe/mick/mick.cpp
@@ -90,7 +90,7 @@ int main(int argc, char** argv)
 	if (command == "generate-keypair")
 	{
 		if (!exists(parent_path(key_path)))
-			return help(options, "key-path is not an accessible path!");
+			return help(options, "key-path is not a writable prefix!");
 
 		string pw = get_pw();
 		return mcleece_generate_keypair(key_path.data(), key_path.size(), pw.data(), pw.size());
