4f6200e1c51570786f4a90669315baf092e40d98
sz <sz@galacticicecube.com>
2020-11-20 04:38:30 -0600

    Point at our in-source SimpleFIPS202 header

--- src/third_party_lib/mceliece8192128/crypto_hash.h
+++ src/third_party_lib/mceliece8192128/crypto_hash.h
@@ -1,6 +1,6 @@
 /* This file uses SHAKE256 implemented in the Keccak Code Package */
 
-#include <libkeccak.a.headers/SimpleFIPS202.h> 
+#include "libkeccak/SimpleFIPS202.h"
 
 #define crypto_hash_32b(out,in,inlen) \
   SHAKE256(out,32,in,inlen)
