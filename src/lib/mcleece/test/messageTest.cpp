2950d9e5822c2f68b7413927aadbe04ef52a1043
sz <sz@recv.cc>
2022-02-28 23:48:12 -0600

    idk

--- src/lib/mcleece/test/messageTest.cpp
+++ src/lib/mcleece/test/messageTest.cpp
@@ -17,8 +17,8 @@ TEST_CASE( "messageTest/testRoundtrip", "[unit]" )
 	MakeTempDirectory tempdir;
 
 	TestHelpers::generate_keypair(tempdir.path() / "test");
-	mcleece::public_key pubk = mcleece::public_key<mcleece::SIMPLE>::from_file(tempdir.path() / "test.pk");
-	mcleece::private_key secret = mcleece::private_key<mcleece::SIMPLE>::from_file(tempdir.path() / "test.sk", "password");
+	mcleece::public_key pubk = mcleece::public_key_simple::from_file(tempdir.path() / "test.pk");
+	mcleece::private_key secret = mcleece::private_key_simple::from_file(tempdir.path() / "test.sk", "password");
 
 	mcleece::session_key session = mcleece::keygen::generate_session_key(pubk);
 	mcleece::nonce n;
